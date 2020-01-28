9f1c2beb476198290b77758c6fc1f5e73778fc8e
Meagan Lang <langmm.astro@gmail.com>
2020-01-28 15:06:26 -0600

    Update wrapper to handle mapping to expected Glyma_ID order and update file connection yaml to use GrCM output and only select expected columns

--- LeafWrap.m
+++ LeafWrap.m
@@ -3,10 +3,24 @@ function LeafAssim=LeafWrap(Env, Einput, Eio)
   global pcfactor;
   pcfactor=1.0/Env('ProteinTotalRatio');
 
+  % Rearrange Einput to use the same order as sample
+  inE = importdata('MeM_input5_0.txt');
+  GeneOrder = inE.textdata(2:size(inE.textdata, 1), 1);
+  GeneNo = size(GeneOrder, 1);
+  GeneMap = containers.Map(Einput{:, 1}, Einput{:, 2});
+  ExpValue = ones(GeneNo, 1);
+  for i=1:GeneNo
+    if isKey(GeneMap, GeneOrder{i, 1})
+      ExpValue(i, 1) = GeneMap(GeneOrder{i, 1});
+    else
+      fprintf("Gene %s is missing.\n", GeneOrder{i, 1});
+    end
+  end
+
   LeafA = Leaf(Env('WeatherRH'), Env('WeatherTemperature'), Env('Air_CO2'), ...
 	       Env('WeatherWind'), Env('Radiation_PAR'), Env('Radiation_NIR'), ...
 	       Env('Radiation_LW'), Env('PhotosynthesisType'), Env('Vcmax25'), ...
-	       Env('Jmax25'), Env('GRNC'), Einput{:, 2}, Eio{:, 2});
+	       Env('Jmax25'), Env('GRNC'), ExpValue, Eio{:, 2});
 
   LeafAssim = LeafA(2);
 end
